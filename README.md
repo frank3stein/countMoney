c2a7ec98152c1a1fcc3712416985e01518efb1eb
Emil Martinov <emil@Emils-MacBook-Pro.local>
2018-10-20 22:20:32 +0200

    First build

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Simple React App to Calculate Bills at the Kassa
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
---
+++ build/precache-manifest.c1ea81b7ca962262e244547772127238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a0c4b6d661e7e756255",
+    "url": "/static/js/main.3a0c4b6d.chunk.js"
+  },
+  {
+    "revision": "14dc7dd24b152667dcb4",
+    "url": "/static/js/1.14dc7dd2.chunk.js"
+  },
+  {
+    "revision": "3a0c4b6d661e7e756255",
+    "url": "/static/css/main.2fb5c682.chunk.css"
+  },
+  {
+    "revision": "11d2ad209c48d75d28c57127883052a6",
+    "url": "/index.html"
+  }
+];
