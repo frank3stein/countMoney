80a8e2410197e7f8825abcad582b956a2ba00d7e
Emil Martinov <emil@Emils-MacBook-Pro.local>
2018-10-20 23:08:02 +0200

    Updates

---
+++ precache-manifest.8996a146f453658536a8a1611d44fad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/countMoney/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3aa1aab39ec368b8b42e",
+    "url": "/countMoney/static/js/runtime~main.3aa1aab3.js"
+  },
+  {
+    "revision": "9df60a67622a6be9646d",
+    "url": "/countMoney/static/js/main.9df60a67.chunk.js"
+  },
+  {
+    "revision": "14dc7dd24b152667dcb4",
+    "url": "/countMoney/static/js/1.14dc7dd2.chunk.js"
+  },
+  {
+    "revision": "9df60a67622a6be9646d",
+    "url": "/countMoney/static/css/main.2fb5c682.chunk.css"
+  },
+  {
+    "revision": "34709b79008e3a00e630da1601f9b929",
+    "url": "/countMoney/index.html"
+  }
+];
